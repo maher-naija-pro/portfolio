35a54f6c515897e4620786a1a8eb16b5f0dca981
maher <mnstudios@hotmail.com>
2024-05-27 14:57:16 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "maher naija's Portfolio",
+  description: "Maher naija Portfolio",
 };
 
 export default function RootLayout({
