0888a9c606358b84456e7608e731b7c9ad2745ea
maher <mnstudios@hotmail.com>
2024-05-27 15:30:24 +0100

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Maher, a Platform Solution architect based in France.
+            Hi ! I&apos;m Maher, a Platform Solution architect based in France.
           </p>
 
           <a href="#about">
