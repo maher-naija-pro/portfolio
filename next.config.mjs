3070cb9875e0c93a2e6f7be446a7f25cbb219f7f
maher <mnstudios@hotmail.com>
2024-05-27 14:42:30 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,23 +2,23 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSentryConfig(withSentryConfig(nextConfig, {
+//export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 // Suppresses source map uploading logs during build
-silent: true,
-org: "javascript-mastery",
-project: "javascript-nextjs",
-}, {
+//silent: true,
+//org: "javascript-mastery",
+//project: "javascript-nextjs",
+//}, {
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 // Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+//widenClientFileUpload: true,
 
 // Transpiles SDK to be compatible with IE11 (increases bundle size)
-transpileClientSDK: true,
+//transpileClientSDK: true,
 
 // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 // This can increase your server load as well as your hosting bill.
@@ -27,47 +27,47 @@ transpileClientSDK: true,
 // tunnelRoute: "/monitoring",
 
 // Hides source maps from generated client bundles
-hideSourceMaps: true,
+//hideSourceMaps: true,
 
 // Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+//disableLogger: true,
 
 // Enables automatic instrumentation of Vercel Cron Monitors.
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-}), {
+//automaticVercelMonitors: true,
+//}), {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "petaresearch-t4",
-project: "javascript-nextjs",
+//org: "petaresearch-t4",
+//project: "javascript-nextjs",
 
 // Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+//silent: !process.env.CI,
 
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 // Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+//widenClientFileUpload: true,
 
 // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 // This can increase your server load as well as your hosting bill.
 // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
 // side errors will fail.
-tunnelRoute: "/monitoring",
+//tunnelRoute: "/monitoring",
 
 // Hides source maps from generated client bundles
-hideSourceMaps: true,
+//hideSourceMaps: true,
 
 // Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+//disableLogger: true,
 
 // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+//automaticVercelMonitors: true,
+//});
