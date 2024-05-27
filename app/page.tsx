f4f5b77ad93792bbd2f5413ccaa04c7e6f913dce
maher <mnstudios@hotmail.com>
2024-05-27 18:49:36 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,12 @@ import Footer from "@/components/Footer";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+//import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
