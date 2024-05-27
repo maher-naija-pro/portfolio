00b7bdb76b5098b261b8c2a0af81f5bc4303cfc1
maher <mnstudios@hotmail.com>
2024-05-27 15:25:34 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link
+          rel="icon"
+          href="https://toppng.com/uploads/preview/upload-to-cloud-free-cloud-ico-115632435885ouurbhg7b.png"
+          sizes="any"
+        />
       </head>
       <body className={inter.className}>
         <ThemeProvider
