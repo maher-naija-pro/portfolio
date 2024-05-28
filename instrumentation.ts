b7ebe4966974d85ee8d96b5bd34404f5051d507c
maher <mnstudios@hotmail.com>
2024-05-28 13:12:40 +0100

    update

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,9 +1,37 @@
+import * as Sentry from "@sentry/nextjs";
+
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    Sentry.init({
+      dsn: "https://6e52f9c6cef0c7ec92b9abeb473f52a3@o4507328419004416.ingest.de.sentry.io/4507328422608976",
+
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+
+      // ...
+
+      // Note: if you want to override the automatic release value, do not set a
+      // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+      // that it will also get attached to your source maps
+    });
   }
 
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+  if (process.env.NEXT_RUNTIME === "edge") {
+    Sentry.init({
+      dsn: "https://6e52f9c6cef0c7ec92b9abeb473f52a3@o4507328419004416.ingest.de.sentry.io/4507328422608976",
+
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+
+      // ...
+
+      // Note: if you want to override the automatic release value, do not set a
+      // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+      // that it will also get attached to your source maps
+    });
   }
 }
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,21 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+
+
+// Make sure adding Sentry options is the last code to run before exporting
+export default withSentryConfig(nextConfig, {
+  org: "petaresearch-t4",
+  project: "javascript-nextjs",
+
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: false, // Can be used to suppress logs
+});
+
+
+
 //export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
