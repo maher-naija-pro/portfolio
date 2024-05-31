5608757cfeefd3c0938b1923142292246758c93a
maher <mnstudios@hotmail.com>
2024-05-31 20:00:15 +0100

    add nav

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
