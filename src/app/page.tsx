eab5aba8f494732c9f4d21923ad6bdade8165de6
maher <mnstudios@hotmail.com>
2024-06-02 19:13:09 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@
 
 import { navItems } from "@/data";
 
-import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import Nav from "@/components/Nav";
+import Hero from "@/src/app/components/Hero";
+import Grid from "@/src/app/components/Grid";
+import Footer from "@/src/app/components/Footer";
+import Nav from "@/src/app/components/Nav";
 //import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Approach from "@/src/app/components/Approach";
+import Experience from "@/src/app/components/Experience";
+import RecentProjects from "@/src/app/components/RecentProjects";
+import { FloatingNav } from "@/src/app/components/ui/FloatingNavbar";
 //  <Nav />
 // <FloatingNav navItems={navItems} />
 
