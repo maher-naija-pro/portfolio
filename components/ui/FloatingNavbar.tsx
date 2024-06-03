61b67e0e0624e8e0b2215706e31bfff3dc9ccc12
maher <mnstudios@hotmail.com>
2024-06-03 18:47:37 +0100

    update

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -96,9 +96,7 @@ export const FloatingNav = ({
             <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
           </button>
         }
-        {
           <button
-            onClick="window.location.href='https://w3docs.com';"
             className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-10 py-2 rounded-lg"
           >
             <span>Loginn</span>
