deaa06f7f1e629ada178072907994621f13f8f82
maher <mnstudios@hotmail.com>
2024-06-04 20:46:39 +0100

    fix provider

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -9,22 +9,8 @@ export function SignIn() {
           className="bg-gray-800 p-10 rounded shadow-md"
           action={async () => {
             "use server";
-            try {
-              await signIn(provider.id);
-            } catch (error) {
-              // Signin can fail for a number of reasons, such as the user
-              // not existing, or the user not having the correct role.
-              // In some cases, you may want to redirect to a custom error
-              if (error instanceof AuthError) {
-                return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
-              }
 
-              // Otherwise if a redirects happens NextJS can handle it
-              // so you can just re-thrown the error and let NextJS handle it.
-              // Docs:
-              // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
-              throw error;
-            }
+            await signIn(provider.id);
           }}
         >
           <h2 className="text-2xl font-bold mb-4 text-gray-100">Sign In</h2>
