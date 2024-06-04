e68ab3030f0581e0f326cf9fd1f53d1083d6bc9c
maher <mnstudios@hotmail.com>
2024-06-04 20:58:34 +0100

    update

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -4,24 +4,22 @@ import { AuthError } from "next-auth";
 export function SignIn() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-900">
-      {Object.values(providerMap).map((provider) => (
-        <form
-          className="bg-gray-800 p-10 rounded shadow-md"
-          action={async () => {
-            "use server";
+      <form
+        className="bg-gray-800 p-10 rounded shadow-md"
+        action={async () => {
+          "use server";
 
-            await signIn(provider.id);
-          }}
+          await signIn("github");
+        }}
+      >
+        <h2 className="text-2xl font-bold mb-4 text-gray-100">Sign In</h2>
+        <button
+          type="submit"
+          className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-lg"
         >
-          <h2 className="text-2xl font-bold mb-4 text-gray-100">Sign In</h2>
-          <button
-            type="submit"
-            className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-lg"
-          >
-            Sign in with GitHub
-          </button>
-        </form>
-      ))}
+          Sign in with GitHub
+        </button>
+      </form>
     </div>
   );
 }
