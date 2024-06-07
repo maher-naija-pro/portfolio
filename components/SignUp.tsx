fa4c1aa21965b21b448345f522210fd549680be6
maher <mnstudios@hotmail.com>
2024-06-07 12:57:06 +0100

    update

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,39 +1,31 @@
 "use client"
-import { useState } from 'react'
 
-import { z } from 'zod';
-const userSchema = z.object({
+import React from "react";
+import { useForm, type FieldValues } from "react-hook-form";
+ 
 
-  email: z.string().email(),
+export  function SignUp() {
+    const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm();
 
-});
+    const onSubmit = async (data: FieldValues) => {
+    // TODO: submit to server
+    // ...
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-const passwordSchema = z.string().refine(password => password.length >= 8, {
-  message: "Password must be at least 8 characters long",
-  path: ['password'], // indicates this error is about the password field
-});
+    reset();
+  };
 
-function UserForm() {
-  const [formData, setFormData] = useState({  email: '', password: '' });
-  const [errors, setErrors] = useState({});
-  
-    const result = userSchema.safeParse(formData);
-    if (result.success) {
-      // Proceed with form submission
-      console.log(result.data);
-    } else {
-      // Handle validation errors
-      setErrors(result.error.formErrors.fieldErrors);
-    }
-  // The rest of your form goes here
-}
-
-export async function SignUp() {
   return (
     <div className="flex justify-center p-10 rounded shadow-md  items-center h-screen bg-gray-900">
       
 
- <form action={UserForm}
+ <form   onSubmit={handleSubmit(onSubmit)} 
         className="bg-gray-800 p-10 rounded shadow-md  transition duration-300 ease-in-out">
       
           <h2 className="text-2xl font-bold mb-4 text-gray-100">Sign Up</h2>
@@ -41,40 +33,28 @@ export async function SignUp() {
             <label htmlFor="email" className="block text-gray-400 text-sm font-bold mb-2">
               Email
             </label>
-            <input
+          <input
               type="email"
-              id="email"
+            id="email"
+               {...register("email", {
+          required: "Email is required",
+        })}
+              placeholder="your email here"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow  hover:bg-gray-700 hover:text-white"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-400 text-sm font-bold mb-2 hover:bg-gray-700 hover:text-white ">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:bg-gray-700 hover:text-white"
-            />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="password-confirm" className="block text-gray-400 text-sm font-bold mb-2">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="password-confirm"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:bg-gray-700 hover:text-white"
-            />
+          />
+              {errors.email && (
+        <p className="text-red-500">{`${errors.email.message}`}</p>
+      )}
         </div>
-        <div>
+      
+        <div >
+
         <button
           type="submit"
           className="
-          border text-sm font-medium relative  w-full border-neutral-200 dark:border-white/[0.2] text-black dark:text-white center r items-center px-8 py-2 rounded-lg hover:bg-gray-700 hover:text-white focus:bg-gray-600 focus:text-white active:bg-violet-400 active:text-white
-    "
+          border text-sm font-medium relative  w-full border-neutral-200 dark:border-white/[0.2] text-black dark:text-white center r items-center px-8 py-2 rounded-lg hover:bg-gray-700 hover:text-white focus:bg-gray-600 focus:text-white active:bg-violet-400 active:text-white"
         >  
-          <a href="/user/sign-in" className="text-sm font-medium relative  w-fulldark:border-white/[0.2] text-black dark:text-white center px-8 py-2 hover:text-whit focus:text-white active:bg-violet-400 active:text-white">   Create account </a>
+          <a className="text-sm font-medium relative  w-fulldark:border-white/[0.2] text-black dark:text-white center px-8 py-2 hover:text-whit focus:text-white active:bg-violet-400 active:text-white">   Create account </a>
           </button>
           </div>
    </form>
